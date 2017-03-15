13b9fd622bf9cc5278c65e093f014be19c80142c
anton <antons.cornijs@gmail.com>
2017-03-15 23:11:26 +0200

    Initial commit

---
+++ app/src/main/java/com/test/anton/githubtrends/ListAdapter.java
@@ -0,0 +1,8 @@
+package com.test.anton.githubtrends;
+
+/**
+ * Created by Anton on 15.03.2017.
+ */
+
+public class ListAdapter {
+}
---
+++ app/src/main/java/com/test/anton/githubtrends/MainActivity.java
@@ -0,0 +1,32 @@
+package com.test.anton.githubtrends;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recycler_view_github)
+    RecyclerView mRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+        ButterKnife.bind(this);
+    }
+}
